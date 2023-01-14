cadbe352c8515d98f43d6cbecbf889df666f8f5a
elcomandamahmoud <elcomandamahmoud2011@gmail.com>
2023-01-14 16:55:34 +0200

    Add project files.

---
+++ Asignment10/Form1.cs
@@ -0,0 +1,46 @@
+﻿using Asignment10.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asignment10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text)==false)
+            {
+                int grade = int.Parse(textBox1.Text.Trim());
+                MessageBox.Show(new MyFunctions().GetGrade(grade));
+            }
+            else
+            {
+                MessageBox.Show("لا يمكنك ترك مربع الدرجة فارغا");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] names = {"حسن","محمد","أحمد","محمود","يوسف"};
+            int[] grades = { 45, 55, 75, 85, 95 };
+            string[] evaluations = new string[5];
+            for (int i = 0; i <names.Count(); i++)
+            {
+                evaluations[i] = new MyFunctions().GetGrade(grades[i]);
+                MessageBox.Show(names[i]+" " + grades[i]+" " + evaluations[i]);
+            }
+        }
+    }
+}
---
+++ Asignment10/ViewModel/MyFunctions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asignment10.ViewModel
+{
+    public class MyFunctions
+    {
+        public string GetGrade(int Grade)
+        {
+            string Evaluation;
+            if (Grade>=0 && Grade<50)
+            {
+                Evaluation = "للأسف أنت راسب، حظ أوفر في العام القادم";
+            }
+            else if (Grade>=50 && Grade<=65)
+            {
+                Evaluation = "مقبول، حاول تحسين مستواك في العام القادم";
+            }
+            else if (Grade>65 && Grade<=80)
+            {
+                Evaluation = "جيد، ننصحك بالجتهاد لكي تصل إلى تقدير أعلى في العام القادم";
+            }
+            else if (Grade>80 && Grade<=90)
+            {
+                Evaluation = "جيد جدا، أحسنت، لكن ننصحك بالاجتهاد أكثر لتصل إلى تقدير الممتاز في العام القادم";
+            }
+            else if (Grade>90 && Grade<=100)
+            {
+                Evaluation = "ممتاز، أحسنت";
+            }
+            else
+            {
+                Evaluation="من فضلك أدخل قيمة صحيحة بين 0 وا 100";
+            }
+            return Evaluation;
+        }
+    }
+}
